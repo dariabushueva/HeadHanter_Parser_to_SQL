e6649fe56dd0f57a7494f2f134ec44bcf01f38c7
dariabushueva <bushueva84@yandex.ru>
2023-07-12 20:46:12 +0300

    init

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+
+import os
+import time
+from typing import Any
+
+import psycopg2
+
+
+def get_hh_data(hh_params: dict[str, Any], employer_ids: list[str]) -> list[dict[str, Any]]:
+    """ Получение данных о работодателях и вакансиях с помощью API """
+    pass
+
+
+def create_database(db_name: str, db_params: dict) -> None:
+    """ Создает базу данных для сохранения полученных данных о работодателях и вакансиях """
+
+
+def save_data_to_database(data: list[dict[str, Any]], db_name: str, db_params: dict) -> None:
+    """Сохраняет данные о канале и видео в базу данных"""
+    pass
+
